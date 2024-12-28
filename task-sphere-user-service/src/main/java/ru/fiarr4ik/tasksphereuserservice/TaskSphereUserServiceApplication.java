2e0e81a066fe5def09058db62546e50cb3404016
fiarr4ik <fiarr4ik@gmail.com>
2024-12-28 18:38:08 +0700

    feat: добавил пустой модуль для работы с пользователями

---
+++ task-sphere-user-service/src/main/java/ru/fiarr4ik/tasksphereuserservice/TaskSphereUserServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.tasksphereuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSphereUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSphereUserServiceApplication.class, args);
+    }
+
+}
